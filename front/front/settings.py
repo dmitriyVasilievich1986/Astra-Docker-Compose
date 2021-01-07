afc2b10a3e5757bb2256f5f59e66fd984cba7752
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2021-01-07 13:00:52 +0300

    version 1.0

--- front/front/settings.py
+++ front/front/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "ssaxm5n&n%y&skwht#e(ff@j@_7gsh$pl^8sytu#6o8jj3dj5g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
