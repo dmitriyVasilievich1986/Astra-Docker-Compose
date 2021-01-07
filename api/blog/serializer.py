3b00559da2d8605b78dc2848b51e271438d29c9d
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2021-01-08 00:01:11 +0300

    08.01.2021

--- api/blog/serializer.py
+++ api/blog/serializer.py
@@ -4,6 +4,8 @@ from .models import Blog
 
 class BlogSerializer(serializers.ModelSerializer):
     likes_count = serializers.SerializerMethodField("get_likes_count")
+    catalog_name = serializers.SerializerMethodField("get_catalog_name")
+    full_catalog_name = serializers.SerializerMethodField("get_full_catalog_name")
 
     class Meta:
         model = Blog
@@ -16,10 +18,18 @@ class BlogSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
             "likes_count",
+            "catalog_name",
+            "full_catalog_name",
         )
 
     def get_likes_count(self, obj, *args, **kwargs):
         return obj.likes.count()
+
+    def get_catalog_name(self, obj, *args, **kwargs):
+        return obj.catalog.name
+
+    def get_catalog_name(self, obj, *args, **kwargs):
+        return obj.catalog.full_catalog.name
 
     def update(self, instance, validated_data, *args, **kwargs):
         instance.name = validated_data.get("name", instance.name)
