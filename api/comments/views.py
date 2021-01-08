489911e860a60938a75a448ac87e6917c06e5d8a
dmitriy <dmitriyvasil@gmail.com>
2021-01-08 16:10:26 +0300

    08.01.2021

--- api/comments/views.py
+++ api/comments/views.py
@@ -13,7 +13,8 @@ class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comments.objects.all()
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(user=request.user, data=request.data)
+        serializer = self.get_serializer(data=request.data)
+        serializer.user = request.user
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
