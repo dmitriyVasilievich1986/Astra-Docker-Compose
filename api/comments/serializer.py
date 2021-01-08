fcdb42c2b9cf9f3d213dacefc645198b1d260d3a
dmitriy <dmitriyvasil@gmail.com>
2021-01-08 16:25:03 +0300

    08.01.2021

--- api/comments/serializer.py
+++ api/comments/serializer.py
@@ -5,7 +5,7 @@ from .models import Comments
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
-        fields = ["id", "text", "owner", "parent"]
+        fields = ["id", "text", "owner", "parent", "user"]
 
     def update(self, instance, validated_data, *args, **kwargs):
         instance.text = validated_data.get("text", instance.text)
